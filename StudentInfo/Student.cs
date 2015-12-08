672b1ede6d0e990d81f7620dcd9fe16c644d5ddc
Cosmos <ezio.lundell@hotmail.com>
2015-12-08 23:55:13 +0100

    First publish
    Adding the code for the project.

---
+++ StudentInfo/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{
+    class Student
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+        public enum genderType { Male, Female };
+        private genderType gender;
+        public genderType Gender
+        {
+            get
+            {
+                return gender;
+            }
+        }
+        private Uri imageLink;
+        public Uri ImageLink
+        {
+            get
+            {
+                return imageLink;
+            }
+        }
+
+        public Student(string firstName, string lastName, genderType gender, int age, Uri imageLink)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.age = age;
+            this.imageLink = imageLink;
+        }
+    }
+}
---
+++ StudentInfo/StudentList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{
+    class StudentList
+    {
+        List<Student> students = new List<Student>();
+
+        public void AddStudent(string firstName, string lastName, Student.genderType gender, int age, Uri imageLink)
+        {
+            students.Add(new Student(firstName, lastName, gender, age, imageLink));
+        }
+    }
+}
